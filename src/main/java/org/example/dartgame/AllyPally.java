d86dd267af5faafdbdbd67a2acca8d586e2dc192
Levin Nussbaum <levin.nussbaum@ict-campus.net>
2024-01-18 14:40:01 +0100

    Fertig Menu, Fast fertig Spieler erstellen, Ally Pallly angefangen

--- src/main/java/org/example/dartgame/AllyPally.java
+++ src/main/java/org/example/dartgame/AllyPally.java
@@ -1,4 +1,26 @@
 package org.example.dartgame;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+import static javafx.application.Application.launch;
+
 public class AllyPally {
+
+    public AllyPally(){
+        //Grundriss erstellen
+        BorderPane screen = new BorderPane();
+        Scene main = new Scene(screen, 500, 500);
+        GridPane gridTop = new GridPane();
+        GridPane gridBottom = new GridPane();
+        ChoiceBox mode = new ChoiceBox();
+        ChoiceBox difficulty = new ChoiceBox();
+    }
+
 }
--- src/main/java/org/example/dartgame/Menue.java
+++ src/main/java/org/example/dartgame/Menue.java
@@ -6,33 +6,95 @@ import javafx.event.EventHandler;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+
+import java.util.Objects;
 
 import static javafx.application.Application.launch;
 
 public class Menue extends Application {
-    public void start(Stage primaryStage) {
-        GridPane main = new GridPane();
-        Scene ground = new Scene(main, 1300, 800, Color.GREY);
 
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+
+        //Grundriss erstellen
+        BorderPane border = new BorderPane();
+        GridPane main = new GridPane();
+        GridPane top = new GridPane();
+        //Node in Stylesheet einbinden
+        border.getStyleClass().add("top");
+        Scene ground = new Scene(border, 1300, 800, Color.BLACK);
+
+        //Nodes erstellen
         Button dart = new Button("Dart spielen");
         Button player = new Button("Spieler erstellen");
+        Label title = new Label("Ally Pally 4life");
+        title.getStyleClass().add("title");
 
-        main.add(dart, 700, 700);
-        main.add(player, 200, 200);
+        //main-grid Grösse und anzhal definieren
+        main.getColumnConstraints().add(new ColumnConstraints(260));
+        main.getColumnConstraints().add(new ColumnConstraints(260));
+        main.getColumnConstraints().add(new ColumnConstraints(260));
+        main.getColumnConstraints().add(new ColumnConstraints(260));
+        main.getColumnConstraints().add(new ColumnConstraints(260));
+        //+
+        main.getRowConstraints().add(new RowConstraints(200));
 
-        primaryStage.setTitle("Nussi isch ume!");
+        //Grösse festlegen
+        dart.setPrefSize(260,200);
+        player.setPrefSize(260,200);
+        title.setPrefSize(500,200);
+
+        //Nodes eibinden in grid
+        main.add(dart, 3,0);
+        main.add(player, 1, 0);
+
+        //Grids in BorderPane einbinden
+        border.setTop(top);
+        border.setCenter(main);
+
+        //Grid Grösse und anzahl bestimmen
+        top.getRowConstraints().add(new RowConstraints(300));
+        //+
+        top.getColumnConstraints().add(new ColumnConstraints(400));
+        top.getColumnConstraints().add(new ColumnConstraints(500));
+        top.getColumnConstraints().add(new ColumnConstraints(400));
+
+        //Titel einbinden in grid
+        top.add(title, 1,0);
+
+        //player-button click -> andere screen zeigen
+        player.setOnAction(event ->{
+            new PlayerScreen(primaryStage);
+        });
+
+        //Stylesheet einbinden
+        ground.getStylesheets().add(getClass().getResource("Stylesheet.css").toExternalForm());
+
+        //Stage bennenen und zeigen
+        primaryStage.setTitle("Darts Menue");
         primaryStage.setScene(ground);
         primaryStage.show();
+
     }
 
     public static void main(String[] args) {
         launch();
     }
+
+
+
+
+
+
+
+
+
 }
